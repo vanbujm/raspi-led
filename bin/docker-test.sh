36c8303abee6857dcfecc6afe3361c48eea34e37
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-15 13:08:58 +0800

    Added tests in an arm emulated container

---
+++ bin/docker-test.sh
@@ -0,0 +1,4 @@
+set -uo pipefail
+BALENA_IMG=balenalib/raspberrypi3-node:$(node --version | grep -o -e '[0-9][0-9].[0-9][0-9]')
+docker pull "$BALENA_IMG"
+docker run -v "$(pwd)":/usr/app -w /usr/app -v "$(yarn cache dir)":/yarn-cache "$BALENA_IMG" bash -c "yarn --cache-folder /yarn-cache && yarn test:ci"
