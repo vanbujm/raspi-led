5553ba4bf2f3f24c990fbf08fac5338f9dee4bf6
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-15 14:39:55 +0800

    trying to get emulation working

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -9,4 +9,4 @@ BALENA_IMG=balenalib/raspberrypi3-node:$(node --version | grep -o -e '[0-9][0-9]
 echo "Pulling image: $BALENA_IMG"
 
 docker pull "$BALENA_IMG"
-docker run -v "$(pwd)":/usr/app -w /usr/app "$BALENA_IMG" /bin/bash -c "yarn && yarn test:ci"
+docker run --rm --privileged -v "$(pwd)":/usr/app -w /usr/app "$BALENA_IMG" /bin/bash -c "yarn && yarn test:ci"
