568f06616c795442a4226a14897e077d7c9b25cc
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-15 13:55:28 +0800

    removed -v typo

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -9,4 +9,4 @@ BALENA_IMG=balenalib/raspberrypi3-node:$(node --version | grep -o -e '[0-9][0-9]
 echo "Pulling image: $BALENA_IMG"
 
 docker pull "$BALENA_IMG"
-docker run -v "$(pwd)":/usr/app -w /usr/app -v "$BALENA_IMG" bash -c "yarn && yarn test:ci"
+docker run -v "$(pwd)":/usr/app -w /usr/app "$BALENA_IMG" bash -c "yarn && yarn test:ci"
