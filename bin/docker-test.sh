8ef1b3afd62c4356e48010f320463141623c178e
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-15 13:15:51 +0800

    cleaned up script and add bash tag

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 set -uo pipefail
+
 BALENA_IMG=balenalib/raspberrypi3-node:$(node --version | grep -o -e '[0-9][0-9].[0-9][0-9]')
+
 docker pull "$BALENA_IMG"
 docker run -v "$(pwd)":/usr/app -w /usr/app -v "$(yarn cache dir)":/yarn-cache "$BALENA_IMG" bash -c "yarn --cache-folder /yarn-cache && yarn test:ci"
