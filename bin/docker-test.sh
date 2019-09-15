9b9a6a9c14418b05be486dde0b73a87c55d51884
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-15 16:18:41 +0800

    trying to get emulation working 12

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -3,11 +3,13 @@
 set -uo pipefail
 
 node --version
-node --version | grep -o -e '[0-9][0-9].[0-9][0-9]'
-BALENA_IMG=balenalib/raspberrypi3-node:$(node --version | grep -o -e '[0-9][0-9].[0-9][0-9]')
+regexp="^v([0-9]*\.[0-9]*)"
+[[ $(node --version) =~ $regexp ]]
+BALENA_IMG=balenalib/raspberrypi3-node:${BASH_REMATCH[1]}
 YARN_CACHE_DIR=$(yarn cache dir)
 
 export YARN_CACHE_DIR
 
 echo "$BALENA_IMG $YARN_CACHE_DIR"
 docker build -t vanbujm/raspi-led-build:latest --build-arg BALENA_IMG="$BALENA_IMG" .
+#
