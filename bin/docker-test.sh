f0fdd3d402f2b981f16365e3e57f43d9b17f0597
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-15 13:33:56 +0800

    fixing bash script 3

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -3,7 +3,7 @@
 set -uo pipefail
 
 BALENA_IMG=balenalib/raspberrypi3-node:$(node --version | grep -o -e '[0-9][0-9].[0-9][0-9]')
-echo "Pulling image: $($BALENA_IMG)"
+echo "Pulling image: $BALENA_IMG"
 
-docker pull "$($BALENA_IMG)"
-docker run -v "$(pwd)":/usr/app -w /usr/app -v "$(yarn cache dir)":/yarn-cache "$($BALENA_IMG)" bash -c "yarn --cache-folder /yarn-cache && yarn test:ci"
+docker pull "$BALENA_IMG"
+docker run -v "$(pwd)":/usr/app -w /usr/app -v "$(yarn cache dir)":/yarn-cache "$BALENA_IMG" bash -c "yarn --cache-folder /yarn-cache && yarn test:ci"
