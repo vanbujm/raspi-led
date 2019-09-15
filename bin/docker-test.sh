d475133143caa98aaa9f1594a28db39ddcb0dce8
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-15 14:51:12 +0800

    trying to get emulation working 3

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -7,7 +7,7 @@ node --version | grep -o -e '[0-9][0-9].[0-9][0-9]'
 
 BALENA_IMG=balenalib/raspberrypi3-node:$(node --version | grep -o -e '[0-9][0-9].[0-9][0-9]')
 echo "Pulling image: $BALENA_IMG"
-
+echo uname -m
 docker pull "$BALENA_IMG"
 echo "docker run --rm --privileged -v $(pwd):/usr/app -w /usr/app $BALENA_IMG /bin/bash -c yarn && yarn test:ci"
 docker run --rm --privileged --entrypoint=/usr/bin/qemu-arm-static -v "$(pwd)":/usr/app -w /usr/app "$BALENA_IMG" /bin/bash -c "yarn && yarn test:ci"
