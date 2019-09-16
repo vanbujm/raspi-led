20241ca41a680492a67633459192ba62aec45d68
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-16 09:59:18 +0800

    Removed superfluous commands

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -11,5 +11,4 @@ YARN_CACHE_DIR=$(yarn cache dir)
 
 export YARN_CACHE_DIR
 
-echo "$BALENA_IMG $YARN_CACHE_DIR"
 docker build -t vanbujm/raspi-led-build:latest --build-arg BALENA_IMG="$BALENA_IMG" .
